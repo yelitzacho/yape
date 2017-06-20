3a8baa2099ddced219b5dd7568fb19b6e9be6ec2
Yelitza Cho <yelitza.choque@gmail.com>
2017-06-20 15:25:42 -0500

    Estructura de la página de inicio

---
+++ public/js/welcome.js
@@ -0,0 +1,32 @@
+// $(document).ready(function(){
+
+// 	var container = $('<div class="container"></div>');
+// 	var row = $('<div class="row"></div>');
+// 	var col = $('<div class="col s12"></div>');
+// 	var carousel = $('<div class="carousel carousel-slider center" data-indicators="true"></div>');
+// 	var carouselItem1 = $('<div class="carousel-item" href="#one!"></div>');
+// 	var imgItem1 = $('<img src="img/icons/icon-people.png" alt="icon people">');
+// 	var titleItem1 = $('<h3>Paga a tus amigos</h3>');
+// 	var descriptionItem1 = $('<p class="">Paga a quien quieras desde donde quieras, sin usar efectivo.</p>');
+// 	var carouselItem2 = $('<div class="carousel-item" href="#two!"></div>');
+// 	var imgItem2 = $('<img src="img/icons/happy-person.png" alt="happy person">');
+// 	var titleItem2 = $('<h3>Sin número de cuenta</h3>');
+// 	var descriptionItem2 = $('<p class="">Elige a quién pagar desde tu lista de contactos.</p>');
+// 	var carouselItem3 = $('<div class="carousel-item" href="#three!"></div>');
+// 	var imgItem3 = $('<img src="img/icons/group-people.png" alt="group people">');
+// 	var titleItem3 = $('<h3>Gratis y Seguro</h3>');
+// 	var descriptionItem3 = $('<p class="">La transferencia es inmediata y gratuita de una cuenta a otra.</p>');
+// 	var button = $('<a class="col s12 btn center-block button-yellow">REGISTRARME</a>');
+
+// 	carouselItem1.append(imgItem1, titleItem1, descriptionItem1);
+// 	carouselItem2.append(imgItem2, titleItem2, descriptionItem2);
+// 	carouselItem3.append(imgItem3, titleItem3, descriptionItem3);
+// 	carousel.append(carouselItem1, carouselItem2, carouselItem3);
+// 	col.append(carousel, button);
+// 	row.append(col);
+// 	container.append(row);
+
+// 	$('#root').append(container);
+// 	$('.carousel.carousel-slider').carousel({fullWidth: true});
+
+// });
